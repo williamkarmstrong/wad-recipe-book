a0e55184e92ae291cbedd6e33ee57e65c5ef4bb9
William Armstrong <2769714a@student.gla.ac.uk>
2025-03-01 15:57:05 +0000

    new app created

---
+++ recipes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from recipes import views
+
+app_name = 'recipes'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- wad_recipe_book/urls.py
+++ wad_recipe_book/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from recipes import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('recipes/', include('recipes.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
