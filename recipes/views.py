4cc9f830312fc496c198908f551a7a45e1132aab
“Ostap <2951499b@student.gla.ac.uk>
2025-03-27 18:02:54 +0000

    test

--- recipes/views.py
+++ recipes/views.py
@@ -135,7 +135,7 @@ def register(request):
         user_form = UserForm()
         profile_form = UserProfileForm()
 
-    return render(request, 'recipes/login.html', context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+    return render(request, 'recipes/register.html', context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
   
 def user_login(request):
